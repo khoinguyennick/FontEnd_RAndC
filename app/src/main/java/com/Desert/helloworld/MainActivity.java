5e2d310397e6f97584b05342530bb1a61d8a0e96
khoinguyennick <khoinguyen.nick@gmail.com>
2019-03-22 11:31:00 +0700

    ... comeback

--- app/src/main/java/com/Desert/helloworld/MainActivity.java
+++ app/src/main/java/com/Desert/helloworld/MainActivity.java
@@ -1,33 +1,48 @@
 package com.Desert.helloworld;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView logoImageView;
-    private ImageView jobImageView;
-    private ImageView profileImageView;
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.logoImageView = findViewById(R.id.logoImageView);
-        this.jobImageView = findViewById(R.id.jobImageView);
-        this.profileImageView = findViewById(R.id.profileImageView);
+        assert getSupportActionBar() != null;   //null check
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);   //show back button
 
-        this.profileImageView.setOnClickListener(view -> gotoProfile());
-        this.logoImageView.setOnClickListener(view ->gotoCvLayout());
+
+        ImageView jobImageView = (ImageView)findViewById(R.id.jobImageView);
+        jobImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,JobsActivity.class);
+                startActivityForResult(intent,0);
+                
+            }
+        });
+        ImageView profileImageView = (ImageView)findViewById(R.id.profileImageView);
+        profileImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,ProfileActivity.class);
+                startActivityForResult(intent,0);
+            }
+        });
+
+
     }
 
-    private void gotoProfile() {
-        setContentView(R.layout.profile_layout);
-    }
-    private void gotoCvLayout(){
-        setContentView(R.layout.cvlayout);
-    }
 
 }
